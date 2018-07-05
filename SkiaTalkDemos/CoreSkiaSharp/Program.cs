c95d308031db020dfa0a93ef6e9dae2bc4601b21
Taylor Wilken <twilken@works24.com>
2018-07-05 16:31:20 -0500

    Initial commit, shell projects created.

---
+++ SkiaTalkDemos/CoreSkiaSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CoreSkiaSharp
+{
+    using System.Diagnostics;
+    using System.Threading;
+    using System.Timers;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch jobTimer = new Stopwatch();
+            jobTimer.Start();
+            Console.WriteLine("Drawing Test Images with Skia...");
+            Thread.Sleep(876);
+            Console.WriteLine("Drawing Test Complete");
+            jobTimer.Stop();
+            Console.WriteLine("Took {0:F2} seconds. Any key to exit.", jobTimer.Elapsed.TotalSeconds);
+            Console.ReadKey();
+        }
+    }
+}
